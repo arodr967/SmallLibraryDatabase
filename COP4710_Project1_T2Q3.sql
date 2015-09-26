4371acb1e445bf8cb9ab708422ebcbf0bf595f9d
Alicia Rodriguez <aliciarodriguez@alicias-mbp.att.net>
2015-09-25 21:09:40 -0400

    Inserted data in the new columns and changed the num_copies type to integer.

--- COP4710_Project1_T2Q3.sql
+++ COP4710_Project1_T2Q3.sql
@@ -1,5 +1,13 @@
 -- Task 2: Managing the database using PostgreSQL.
 -- Query 3: Add one column called "num_copies" to the Books table and one column called "Sex" to the Readers table.
 
-ALTER TABLE Books ADD COLUMN num_copies text;
-ALTER TABLE Readers ADD COLUMN Sex text;
+ALTER TABLE Books ADD COLUMN num_copies integer;
+ALTER TABLE Readers ADD COLUMN sex text;
+
+-- Insert the data in the new columns.
+
+INSERT INTO Books (num_copies) VALUES
+(10), (10), (10), (10), (10);
+
+INSERT INTO Readers (sex) VALUES
+('Female'), ('Male'), ('Male'), ('Male'), ('Female');
