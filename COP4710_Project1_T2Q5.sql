ab4a75b3006d608f294a56e4823ca598fef9da95
Alicia Rodriguez <aliciarodriguez@alicias-mbp.att.net>
2015-09-26 01:33:11 -0400

    Modified update of the constraint because an error occurred.
    [WARNING  ] UPDATE Books SET price = (price > 0 AND price < 15)            ERROR:  column "price" is of type numeric but expression is of type boolean            LINE 1: UPDATE Books SET price = (price > 0 AND price < 15)                                              ^            HINT:  You will need to rewrite or cast the expression.
    
    Fixed issue by dropping the constraint and re-adding it.

--- COP4710_Project1_T2Q5.sql
+++ COP4710_Project1_T2Q5.sql
@@ -1,4 +1,6 @@
 -- Task 2: Managing the database using PostgreSQL.
 -- Query 5: Change the check constraint in the table Books to constraint the entire price less than 15
 
-UPDATE Books SET price = (price > 0 AND price < 15);
+ALTER TABLE Books DROP CONSTRAINT check_price;
+
+ALTER TABLE Books ADD CONSTRAINT check_price CHECK (price > 0 AND price < 15);
