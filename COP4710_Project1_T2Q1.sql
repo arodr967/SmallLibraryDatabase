0ea6f8b26c643442b3f6f6117afa4c705ee64b15
Alicia Rodriguez <aliciarodriguez@alicias-mbp.att.net>
2015-09-26 00:49:23 -0400

    Another error occurs:
    ERROR:  insert or update on table "loanlist" violates foreign key constraint "loanlist_isbn_fkey1"
                DETAIL:  Key (isbn,copy_id)=(0375842209,3) is not present in table "copies".
    
    This means that I need to either add that extra copy of the book to the Copies table OR not include that in the LoanList table.

--- COP4710_Project1_T2Q1.sql
+++ COP4710_Project1_T2Q1.sql
@@ -5,4 +5,8 @@ INSERT INTO LoanList (isbn, reader_id, copy_id, out_date, due_date, in_date) VAL
 ('159514188X', 4, 2, '09/01/2011', '09/28/2011', '09/05/2011'),
 ('0375842209', 2, 3, '09/07/2011', '10/19/2011', NULL);
 
--- An error occurs when the copy_id of isbn: 0375842209, is 1 because it violates the unique constraint. Therefore, the copy_id has to be changed to 3, because it's the 3rd copy of that isbn, checked out of the library.
+-- An error occurs when the copy_id of isbn: 0375842209, is 1 because it violates the unique constraint. 
+-- Therefore, the copy_id has to be changed to 3, because it's the 3rd copy of that isbn, checked out of the library.
+
+-- Another error occurs when changing the number to 3. The 3rd copy of the book is not in the
+-- Copies table, therefore unable to add it to the LoanList, because there is not an available copy.
