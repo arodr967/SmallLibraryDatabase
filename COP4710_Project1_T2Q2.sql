405467ee9cfc7112dc9e7979fb310915c92152cd
Alicia Rodriguez <aliciarodriguez@alicias-mbp.att.net>
2015-09-26 00:55:28 -0400

    Fixed syntax error.

--- COP4710_Project1_T2Q2.sql
+++ COP4710_Project1_T2Q2.sql
@@ -1,4 +1,4 @@
 -- Task 2: Managing the database using PostgreSQL.
 -- Query 2: Delete the records from the table LoanList where in_date is not Null.
 
-DELETE FROM LoanList WHERE in_date SET NOT NULL;
+DELETE FROM LoanList WHERE in_date IS NOT NULL;
