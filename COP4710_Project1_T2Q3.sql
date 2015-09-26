01e26d171d3f2d948ea80aadeff4306ebd016d9c
Alicia Rodriguez <aliciarodriguez@alicias-mbp.att.net>
2015-09-26 01:20:40 -0400

    Provided correct syntax on how to insert onto a newly added column.

--- COP4710_Project1_T2Q3.sql
+++ COP4710_Project1_T2Q3.sql
@@ -6,8 +6,21 @@ ALTER TABLE Readers ADD COLUMN sex text;
 
 -- Insert the data in the new columns.
 
-INSERT INTO Books (num_copies) VALUES
-(10), (10), (10), (10), (10);
+-- Update the Books table.
 
-INSERT INTO Readers (sex) VALUES
-('Female'), ('Male'), ('Male'), ('Male'), ('Female');
+UPDATE Books
+SET num_copies = 3 -- set a value to be inserted in the num_copies column
+WHERE book_id = 1; -- and put that value where the PK, books_id is equals to 1
+
+UPDATE Books SET num_copies = 0 WHERE book_id = 2;
+UPDATE Books SET num_copies = 3 WHERE book_id = 3;
+UPDATE Books SET num_copies = 1 WHERE book_id = 4;
+UPDATE Books SET num_copies = 0 WHERE book_id = 5;
+
+-- Update the Readers table.
+
+UPDATE Readers SET sex = 'FEMALE' WHERE reader_id = 1;
+UPDATE Readers SET sex = 'MALE' WHERE reader_id = 2;
+UPDATE Readers SET sex = 'MALE' WHERE reader_id = 3;
+UPDATE Readers SET sex = 'MALE' WHERE reader_id = 4;
+UPDATE Readers SET sex = 'FEMALE' WHERE reader_id = 5;
