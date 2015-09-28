5318becaff2ddca047e382a382f8313e0efeae6f
Alicia Rodriguez <arodr967@fiu.edu>
2015-09-28 18:56:08 -0400

    Update the insert
    Removed: ('0375842209', 2, 1, '09/07/2011', '10/19/2011', NULL);
    Must add that in T2Q2, when the not Null in_date are removed from the LoanList table.

--- COP4710_Project1_T2Q1.sql
+++ COP4710_Project1_T2Q1.sql
@@ -1,16 +1,10 @@
 -- Task 2: Managing the database using PostgreSQL.
 -- Query 1: Insert two more records to table LoanList by using one query.
 
-INSERT INTO Copies (isbn, copy_id, title) VALUES
-('0375842209', 3, 'The Book Thief');
-
 INSERT INTO LoanList (isbn, reader_id, copy_id, out_date, due_date, in_date) VALUES
-('159514188X', 4, 2, '09/01/2011', '09/28/2011', '09/05/2011'),
-('0375842209', 2, 3, '09/07/2011', '10/19/2011', NULL);
+('159514188X', 4, 2, '09/01/2011', '09/28/2011', '09/05/2011');
 
 -- An error occurs when the copy_id of isbn: 0375842209, is 1 because it violates the unique constraint. 
--- Therefore, the copy_id has to be changed to 3, because it's the 3rd copy of that isbn, checked out of the library.
-
--- Another error occurs when changing the number to 3. The 3rd copy of the book is not in the
--- Copies table, therefore unable to add it to the LoanList, because there is not an available copy.
--- In order to fix this issue, another copy of isbn: 0375842209, must be added to the Copies table.
+-- In order to fix this, we must add the isbn: 0375842209 after Query 2 is performed, when all of the
+-- not Null, in_dates are deleted. Therefore, we are able to add that book to the LoanList because
+-- copy_id 1, was used but then already returned before being checked out again.
