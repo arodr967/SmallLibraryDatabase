dea31d60292f283481449a5fe81ab224fb174eaf
Alicia Rodriguez <arodr967@fiu.edu>
2015-09-28 18:59:08 -0400

    Added to insert to LoanList
    Added the insert after the deletion of the not null in_date

--- COP4710_Project1_T2Q2.sql
+++ COP4710_Project1_T2Q2.sql
@@ -2,3 +2,8 @@
 -- Query 2: Delete the records from the table LoanList where in_date is not Null.
 
 DELETE FROM LoanList WHERE in_date IS NOT NULL;
+
+-- Now we can add the book.
+
+INSERT INTO LoanList (isbn, reader_id, copy_id, out_date, due_date, in_date) VALUES
+('0375842209', 2, 1, '09/07/2011', '10/19/2011', NULL);
