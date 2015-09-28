59d2bec558aed6d62b2c894dfdf16d01e12230bc
Alicia Rodriguez <arodr967@fiu.edu>
2015-09-28 18:47:21 -0400

    Updated the books and readers table
    Updated the books and readers table, with respect to project 2.

--- COP4710_Project1_T2Q3.sql
+++ COP4710_Project1_T2Q3.sql
@@ -9,18 +9,18 @@ ALTER TABLE Readers ADD COLUMN sex text;
 -- Update the Books table.
 
 UPDATE Books
-SET num_copies = 3 -- set a value to be inserted in the num_copies column
+SET num_copies = 2 -- set a value to be inserted in the num_copies column
 WHERE book_id = 1; -- and put that value where the PK, books_id is equals to 1
 
-UPDATE Books SET num_copies = 0 WHERE book_id = 2;
+UPDATE Books SET num_copies = 4 WHERE book_id = 2;
 UPDATE Books SET num_copies = 3 WHERE book_id = 3;
-UPDATE Books SET num_copies = 1 WHERE book_id = 4;
-UPDATE Books SET num_copies = 0 WHERE book_id = 5;
+UPDATE Books SET num_copies = 2 WHERE book_id = 4;
+UPDATE Books SET num_copies = 5 WHERE book_id = 5;
 
 -- Update the Readers table.
 
-UPDATE Readers SET sex = 'FEMALE' WHERE reader_id = 1;
-UPDATE Readers SET sex = 'MALE' WHERE reader_id = 2;
-UPDATE Readers SET sex = 'MALE' WHERE reader_id = 3;
-UPDATE Readers SET sex = 'MALE' WHERE reader_id = 4;
-UPDATE Readers SET sex = 'FEMALE' WHERE reader_id = 5;
+UPDATE Readers SET sex = 'F' WHERE reader_id = 1;
+UPDATE Readers SET sex = 'F' WHERE reader_id = 2;
+UPDATE Readers SET sex = 'M' WHERE reader_id = 3;
+UPDATE Readers SET sex = 'M' WHERE reader_id = 4;
+UPDATE Readers SET sex = 'F' WHERE reader_id = 5;
