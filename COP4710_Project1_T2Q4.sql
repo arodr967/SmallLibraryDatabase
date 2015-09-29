5008b722aeb3de227a285184f6f6cab4bea8f8ee
Alicia Rodriguez <arodr967@fiu.edu>
2015-09-28 21:14:50 -0400

    Updated wording

--- COP4710_Project1_T2Q4.sql
+++ COP4710_Project1_T2Q4.sql
@@ -1,4 +1,4 @@
 -- Task 2: Managing the database using PostgreSQL.
 -- Query 4: Export the LoanList table as Loanlist.csv by using SQL command "COPY".
 
-COPY LoanList TO '/sql/Loanlist.csv' WITH DELIMITER '|';
+COPY LoanList TO '/arodr967/Loanlist.csv' WITH DELIMITER '|';
